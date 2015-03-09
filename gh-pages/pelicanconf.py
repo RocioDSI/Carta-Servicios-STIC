dbb39f80c20caa2a38e45535c2d6e076dd2c93b7
Rocío Rodríguez <alu0100592832@ull.edu.es>
2015-03-09 09:56:09 +0000

    Cambiando url en el pelicanconf

--- gh-pages/pelicanconf.py
+++ gh-pages/pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Universidad de La Laguna'
 SITENAME = u'Carta de Servicios'
-# SITEURL = '<site_url>'
+SITEURL = 'Carta-Servicios-STIC/'
 
 PATH = 'content'
 
