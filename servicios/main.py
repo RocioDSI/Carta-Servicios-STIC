7ad9156f028528d6734f3ca6e4e11c453199f0ca
Hector Royo <hectorroyo92@gmail.com>
2015-03-13 10:53:54 +0000

    Arreglando problema con rama

--- servicios/main.py
+++ servicios/main.py
@@ -2,11 +2,15 @@ import funcionesxml
 import generacionpaginas
 import os
 
-os.system("rm -r -f servicios_stic/templates/web/")
-os.system("mkdir servicios_stic/templates/web/")
-funcionesxml.inicializacion()
-generacionpaginas.generahtmlmenu()
-generacionpaginas.generaurls()
-generacionpaginas.generahtmlindex()
-generacionpaginas.generaviews()
-generacionpaginas.generaplantillas()
+def main():
+ os.system("rm -r -f servicios_stic/templates/web/")
+ os.system("mkdir servicios_stic/templates/web/")
+ funcionesxml.inicializacion()
+ generacionpaginas.generahtmlmenu()
+ generacionpaginas.generaurls()
+ generacionpaginas.generahtmlindex()
+ generacionpaginas.generaviews()
+ generacionpaginas.generaplantillas()
+
+
+main()
