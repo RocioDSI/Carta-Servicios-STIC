20ef05fc3d4e33652c0e1733e763ac5797566a35
Rocío Rodríguez <alu0100592832@ull.edu.es>
2015-04-06 13:19:39 +0100

    Mejorando estilos de la pagina upload

--- servicios/servicios_stic/forms.py
+++ servicios/servicios_stic/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class UploadForm(forms.Form):
- Nombre_Universidad_Corto = forms.CharField(max_length=100)
- Nombre_Universidad = forms.CharField(max_length=100)
+ Nombre_Universidad_Corto = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": 'form-control'}))
+ Nombre_Universidad = forms.CharField(max_length=100, widget=forms.TextInput(attrs={"class": 'form-control'}))
  docfile = forms.FileField(
    label='Selecciona el modelo Archimate'
  )   
